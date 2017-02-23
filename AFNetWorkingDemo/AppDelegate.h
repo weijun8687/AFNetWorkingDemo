539dfd46f0740a9462e94aee7c56aba9ff307f52
WJ <wj@wjdemacbook-pro.local>
2017-02-23 19:16:57 +0800

    Initial Commit

---
+++ AFNetWorkingDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AFNetWorkingDemo
+//
+//  Created by WJ on 17/2/23.
+//  Copyright © 2017年 WJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AFNetWorkingDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AFNetWorkingDemo
+//
+//  Created by WJ on 17/2/23.
+//  Copyright © 2017年 WJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
